1369e1982acc23a1108333b1c57919762b573500
rrahn <rene.rahn@fu-berlin.de>
2020-04-02 21:11:21 +0200

    [FEATURE] Add implementation for affine gap computation with traceback.

--- include/seqan3/alignment/pairwise/detail/pairwise_alignment_policy_affine_gaps.hpp
+++ include/seqan3/alignment/pairwise/detail/pairwise_alignment_policy_affine_gaps.hpp
@@ -16,6 +16,7 @@
 
 #include <seqan3/alignment/configuration/align_config_gap.hpp>
 #include <seqan3/alignment/pairwise/detail/affine_cell_proxy.hpp>
+#include <seqan3/alignment/pairwise/detail/concept.hpp>
 #include <seqan3/alignment/scoring/gap_scheme.hpp>
 
 namespace seqan3::detail
@@ -27,7 +28,11 @@ class pairwise_alignment_policy_affine_gaps
 private:
     using traits_type = alignment_configuration_traits<alignment_config_t>;
     using score_type = typename traits_type::score_t;
-    using affine_cell_proxy_t = affine_cell_proxy<std::tuple<score_type, score_type, score_type>>;
+    using trace_type = typename traits_type::trace_t;
+
+    //TODO: Get this generically from some traits function.
+    using affine_score_cell_type = affine_score_proxy<std::tuple<score_type, score_type, score_type>>;
+    using affine_trace_cell_type = affine_trace_proxy<std::tuple<trace_type, trace_type, trace_type>>;
 
 protected:
     //!\brief The score for a gap extension.
@@ -71,7 +76,7 @@ protected:
     //!\}
 
     /*!\brief Initialises the first cell of the alignment matrix in the top left corner of the matrix.
-     * \tparam affine_cell_t The type of the affine cell.
+     * \tparam affine_score_cell_t The type of the affine cell.
      *
      * \param[in] diagonal_score The previous diagonal score, which corresponds to \f$M[i - 1, j - 1]\f$.
      * \param[in] previous_cell The predecessor cell corresponding to the values \f$V[i - 1, j]\f$ and \f$H[i, j -1]\f$.
@@ -102,10 +107,10 @@ protected:
      * \end{cases}
      * \f]
      */
-    template <typename affine_cell_t>
-    auto compute_inner_cell(score_type diagonal_score,
-                            affine_cell_t previous_cell,
-                            score_type const sequence_score) const noexcept
+    template <affine_score_cell affine_score_cell_t>
+    affine_score_cell_type compute_inner_cell(score_type diagonal_score,
+                                              affine_score_cell_t previous_cell,
+                                              score_type const sequence_score) const noexcept
     {
         diagonal_score += sequence_score;
         score_type horizontal_score = previous_cell.horizontal_score();
@@ -127,7 +132,50 @@ protected:
         vertical_score = (vertical_score < tmp) ? tmp : vertical_score;
         horizontal_score = (horizontal_score < tmp) ? tmp : horizontal_score;
 
-        return affine_cell_proxy_t{diagonal_score, horizontal_score, vertical_score};
+        return {diagonal_score, horizontal_score, vertical_score};
+    }
+
+    //!\overload
+    template <affine_score_trace_cell affine_score_trace_cell_t> // We need a concept that helps to overload on these methods.
+    auto compute_inner_cell(score_type diagonal_score,
+                            affine_score_trace_cell_t previous_cell,
+                            score_type const sequence_score) const noexcept
+    {
+        auto [score_cell, trace_cell] = previous_cell;
+        // Now we need to write the kernel that also keeps track of the trace
+        diagonal_score += sequence_score;
+        score_type horizontal_score = score_cell.horizontal_score();
+        score_type vertical_score = score_cell.vertical_score();
+
+        trace_type new_trace = trace_cell.vertical_trace();
+;
+        diagonal_score = (diagonal_score < vertical_score)
+                       ? vertical_score
+                       : (new_trace |= trace_directions::diagonal, diagonal_score);
+        diagonal_score = (diagonal_score < horizontal_score)
+                       ? (new_trace = trace_cell.horizontal_trace(), horizontal_score)
+                       : (new_trace |= trace_cell.horizontal_trace(), diagonal_score);
+
+        // Check if zero cutoff is enabled so that score cannot fall below 0.
+        if constexpr (with_zero_cutoff)
+            diagonal_score = (diagonal_score < 0) ? (new_trace = trace_directions::none, 0) : diagonal_score;
+
+        score_type tmp = diagonal_score + gap_open_score;
+        vertical_score += gap_extension_score;
+        horizontal_score += gap_extension_score;
+
+        trace_type next_vertical_trace;
+        trace_type next_horizontal_trace;
+        // store the vertical_score and horizontal_score value in the next path
+        vertical_score = (vertical_score < tmp)
+                       ? (next_vertical_trace = trace_directions::up_open, tmp)
+                       : (next_vertical_trace = trace_directions::up, vertical_score);
+        horizontal_score = (horizontal_score < tmp)
+                         ? (next_horizontal_trace = trace_directions::left_open, tmp)
+                         : (next_horizontal_trace = trace_directions::left, horizontal_score);
+
+        return std::pair{affine_score_cell_type{diagonal_score, horizontal_score, vertical_score},
+                         affine_trace_cell_type{new_trace, next_horizontal_trace, next_vertical_trace}};
     }
 
     /*!\brief Initialises the first cell of the alignment matrix in the top left corner of the matrix.
@@ -140,13 +188,26 @@ protected:
      * initialised to 0, while the value of the horizontal and vertical matrix are initialised as:
      * \f$V[0, 0] = H[0, 0] = g_o\f$.
      */
-    affine_cell_proxy_t initialise_origin_cell() const noexcept
+    template <affine_score_cell affine_score_cell_t>
+    affine_score_cell_type initialise_origin_cell(affine_score_cell_t && SEQAN3_DOXYGEN_ONLY(cell)) const noexcept
     {
         return {0, (zero_initialise_row) ? 0 : gap_open_score, (zero_initialise_column) ? 0 : gap_open_score};
     }
 
+    //!\overload
+    template <affine_score_trace_cell affine_score_trace_cell_t>
+    auto initialise_origin_cell(affine_score_trace_cell_t && cell) const noexcept
+    {
+        using std::get;
+        return std::pair{
+                initialise_origin_cell(get<0>(cell)),
+                affine_trace_cell_type{trace_directions::none,
+                                       (zero_initialise_row) ? trace_directions::none : trace_directions::left_open,
+                                       (zero_initialise_column) ? trace_directions::none : trace_directions::up_open}};
+    }
+
     /*!\brief Initialises a cell of the first alignment matrix column.
-     * \tparam affine_cell_t The type of the affine cell.
+     * \tparam affine_score_cell_t The type of the affine cell.
      *
      * \param previous_cell The predecessor cell on the same column.
      *
@@ -158,15 +219,28 @@ protected:
      * which is equal to \f$V[i, 0] = M[i, 0] = g_o + g_e * i\f$. The horizontal score is initialised to
      * \f$H[i, 0] = V[i, 0] + g_o\f$ to prohibit extending a gap in the horizontal matrix from \f$H[i, 0]\f$.
      */
-    template <typename affine_cell_t>
-    affine_cell_proxy_t initialise_first_column_cell(affine_cell_t cell) const noexcept
+    template <affine_score_cell affine_score_cell_t>
+    affine_score_cell_type initialise_first_column_cell(affine_score_cell_t && cell) const noexcept
     {
         score_type vertical_score = (zero_initialise_column) ? 0 : cell.vertical_score() + gap_extension_score;
         return {cell.vertical_score(), cell.vertical_score() + gap_open_score, vertical_score};
     }
 
+    //!\overload
+    template <affine_score_trace_cell affine_score_cell_t>
+    auto initialise_first_column_cell(affine_score_cell_t && cell) const noexcept
+    {
+        using std::get;
+
+        return std::pair{
+            initialise_first_column_cell(get<0>(cell)),
+            affine_trace_cell_type{get<1>(cell).vertical_trace(),
+                                   trace_directions::left_open,
+                                   (zero_initialise_column) ? trace_directions::none : trace_directions::up}};
+    }
+
     /*!\brief Initialises the first cell of a alignment matrix column.
-     * \tparam affine_cell_t The type of the affine cell.
+     * \tparam affine_score_cell_t The type of the affine cell.
      *
      * \param cell The predecessor cell on the same row.
      *
@@ -178,13 +252,26 @@ protected:
      * which is equal to \f$H[0, j] = M[0, j] = g_o + g_e * j\f$. The vertical score is initialised to
      * \f$H[0, j] + g_o\f$ to prohibit extending a gap in the vertical matrix from \f$V[0, j]\f$.
      */
-    template <typename affine_cell_t>
-    affine_cell_proxy_t initialise_first_row_cell(affine_cell_t cell) const noexcept
+    template <affine_score_cell affine_score_cell_t>
+    affine_score_cell_type initialise_first_row_cell(affine_score_cell_t && cell) const noexcept
     {
         score_type horizontal_score = (zero_initialise_row) ? 0 : cell.horizontal_score() + gap_extension_score;
         return {cell.horizontal_score(),
                 horizontal_score,
                 cell.horizontal_score() + gap_open_score};
+    }
+
+    //!\overload
+    template <affine_score_trace_cell affine_score_cell_t>
+    auto initialise_first_row_cell(affine_score_cell_t && cell) const noexcept
+    {
+        using std::get;
+
+        return std::pair{
+            initialise_first_row_cell(get<0>(cell)),
+            affine_trace_cell_type{get<1>(cell).horizontal_trace(),
+                                   (zero_initialise_row) ? trace_directions::none : trace_directions::left,
+                                   trace_directions::up_open}};
     }
 };
 } // namespace seqan3::detail
