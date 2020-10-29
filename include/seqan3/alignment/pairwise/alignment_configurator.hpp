e8a12e754bb7184f227d2709b132934f9addeafb
rrahn <rene.rahn@fu-berlin.de>
2020-10-29 23:00:18 +0100

    [FEATURE] Enables the vectorised alignment with end positions.

--- include/seqan3/alignment/pairwise/alignment_configurator.hpp
+++ include/seqan3/alignment/pairwise/alignment_configurator.hpp
@@ -495,13 +495,12 @@ private:
         // Temporarily we will use the new and the old alignment implementation in order to
         // refactor step-by-step to the new implementation. The new implementation will be tested in
         // macrobenchmarks to show that it maintains a high performance.
-        constexpr bool more_than_score = traits_t::compute_end_positions ||
-                                         traits_t::compute_begin_positions ||
-                                         traits_t::compute_sequence_alignment;
+        constexpr bool more_than_end_position = traits_t::compute_begin_positions ||
+                                                traits_t::compute_sequence_alignment;
         // Use old alignment implementation if...
-        if constexpr (traits_t::is_local ||                                                            // it is a local alignment,
-                     (traits_t::is_banded && (more_than_score || traits_t::compute_end_positions)) ||  // banded
-                     (traits_t::is_vectorised && more_than_score))                                     // simd and more than the score.
+        if constexpr (traits_t::is_local ||                               // it is a local alignment,
+                     (traits_t::is_banded && more_than_end_position) ||   // banded
+                     (traits_t::is_vectorised && more_than_end_position)) // simd and more than the score.
         {
             using matrix_policy_t = typename select_matrix_policy<traits_t>::type;
             using gap_policy_t = typename select_gap_policy<traits_t>::type;
