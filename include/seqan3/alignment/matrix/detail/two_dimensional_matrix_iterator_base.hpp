cdccbe5d6a1f459e8537e76d72d2078f0dc2e0d8
rrahn <rene.rahn@fu-berlin.de>
2020-11-15 16:06:50 +0100

    [NOP] Fix include order.

--- include/seqan3/alignment/matrix/detail/two_dimensional_matrix_iterator_base.hpp
+++ include/seqan3/alignment/matrix/detail/two_dimensional_matrix_iterator_base.hpp
@@ -12,13 +12,13 @@
 
 #pragma once
 
+#include <seqan3/std/concepts>
+#include <seqan3/std/ranges>
 #include <type_traits>
 
 #include <seqan3/alignment/matrix/detail/matrix_coordinate.hpp>
 #include <seqan3/core/type_traits/basic.hpp>
 #include <seqan3/core/type_traits/iterator.hpp>
-#include <seqan3/std/concepts>
-#include <seqan3/std/ranges>
 
 namespace seqan3::detail
 {
