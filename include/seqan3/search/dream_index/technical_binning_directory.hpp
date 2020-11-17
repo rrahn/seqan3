946dac930684e288213d16ea3540e5137432a487
Enrico Seiler <enrico.seiler@hotmail.de>
2020-11-17 14:53:47 +0100

    Add missing include

--- include/seqan3/search/dream_index/technical_binning_directory.hpp
+++ include/seqan3/search/dream_index/technical_binning_directory.hpp
@@ -14,6 +14,7 @@
 
 #include <seqan3/alphabet/nucleotide/dna4.hpp>
 #include <seqan3/core/algorithm/detail/execution_handler_parallel.hpp>
+#include <seqan3/core/concept/tuple.hpp>
 #include <seqan3/range/views/chunk.hpp>
 #include <seqan3/range/views/kmer_hash.hpp>
 #include <seqan3/range/views/zip.hpp>
