eb2a44c730daffc730bd56d11cf191a56dc7cafe
René Rahn <rahn@icebear.molgen.mpg.de>
2020-11-20 13:19:53 +0100

    WIP only use pure algorithm to compute the alignment
    This makes the benchmark to only use the internal algorithm without all the sheningans.

--- test/performance/alignment/global_affine_alignment_simd_benchmark_template.hpp
+++ test/performance/alignment/global_affine_alignment_simd_benchmark_template.hpp
@@ -15,6 +15,7 @@
 #include <seqan3/alignment/pairwise/align_pairwise.hpp>
 #include <seqan3/alphabet/aminoacid/aa20.hpp>
 #include <seqan3/alphabet/nucleotide/dna4.hpp>
+#include <seqan3/range/views/chunk.hpp>
 #include <seqan3/range/views/to.hpp>
 #include <seqan3/range/views/zip.hpp>
 #include <seqan3/test/performance/sequence_generator.hpp>
@@ -42,6 +43,41 @@ uint32_t get_number_of_threads()
     return (thread_count == 1) ? thread_count : thread_count >> 1;
 }
 
+namespace sd = seqan3::detail;
+
+template <typename config_t, typename alphabet_t>
+struct alignment_algorithm_for
+{
+private:
+    using traits_t = sd::alignment_configuration_traits<config_t>;
+    using optimum_tracker_policy_t = sd::select_optimum_policy_t<traits_t>;
+    using gap_cost_policy_t = sd::select_recursion_policy_t<traits_t>;
+    using result_builder_policy_t = sd::select_result_builder_policy_t<traits_t>;
+    using scoring_scheme_policy_t = sd::select_scoring_scheme_policy_t<traits_t, alphabet_t>;
+    using alignment_matrix_policy_t = sd::select_alignment_matrix_policy_t<traits_t>;
+    using alignment_logger_policy_t = sd::select_logger_policy_t<traits_t>;
+public:
+    using type = sd::select_alignment_algorithm_t<traits_t,
+                                                  gap_cost_policy_t,
+                                                  scoring_scheme_policy_t,
+                                                  optimum_tracker_policy_t,
+                                                  alignment_logger_policy_t,
+                                                  alignment_matrix_policy_t,
+                                                  result_builder_policy_t>;
+};
+
+template <typename config_t, typename data_t>
+auto configure_alignment_result(config_t const & config, data_t const &)
+{
+    using sequence_pair_t = std::ranges::range_value_t<data_t>;
+    using sequence_t = std::tuple_element_t<0, sequence_pair_t>;
+    using alignment_result_value_t = typename sd::align_result_selector<sequence_t &, sequence_t &, config_t>::type;
+    using alignment_result_t = seqan3::alignment_result<alignment_result_value_t>;
+
+    return config | seqan3::align_cfg::detail::result_type<alignment_result_t>{};
+}
+
+
 // ----------------------------------------------------------------------------
 //  seqan3 pairwise alignment
 // ----------------------------------------------------------------------------
@@ -55,11 +91,20 @@ void seqan3_affine_accelerated(benchmark::State & state, alphabet_t, align_confi
                                                                   sequence_length_variance);
 
     int64_t total = 0;
-    auto accelerate_config = (configs | ...);
+    auto accelerate_config = configure_alignment_result((configs | ...), data);
+
+    using algo_t = typename alignment_algorithm_for<decltype(accelerate_config), alphabet_t>::type;
+    algo_t algo{accelerate_config};
+
+    using score_t = typename std::remove_reference_t<decltype(accelerate_config.get_or(seqan3::align_cfg::score_type<int32_t>{}))>::type;
+
+    constexpr size_t chunk_size = seqan3::simd_traits<seqan3::simd_type_t<score_t>>::length;
+    auto chunked_data = seqan3::views::zip(data, std::views::iota(0)) | seqan3::views::chunk(chunk_size);
+
     for (auto _ : state)
     {
-        for (auto && res : seqan3::align_pairwise(data, accelerate_config))
-            total += res.score();
+        for (auto && chunk : chunked_data)
+            algo(chunk, [&] (auto res) { total += res.score(); });
     }
 
     state.counters["cells"] = seqan3::test::pairwise_cell_updates(data, accelerate_config);
