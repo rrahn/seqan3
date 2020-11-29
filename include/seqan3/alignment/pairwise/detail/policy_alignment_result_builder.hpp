b10922aebaf893a701c34f0d4ab940d102f8fa2f
rrahn <rahn@molgen.mpg.de>
2020-11-29 20:09:13 +0100

    [MISC] Refactor the selection of the result builder policy.
    Adds a dedicated transformation trait to configure the result builder policy.

--- include/seqan3/alignment/pairwise/detail/policy_alignment_result_builder.hpp
+++ include/seqan3/alignment/pairwise/detail/policy_alignment_result_builder.hpp
@@ -12,8 +12,12 @@
 
 #pragma once
 
+#include <seqan3/std/concepts>
+
+#include <seqan3/alignment/matrix/detail/aligned_sequence_builder.hpp>
 #include <seqan3/alignment/pairwise/detail/policy_alignment_algorithm_logger.hpp>
 #include <seqan3/alignment/pairwise/detail/type_traits.hpp>
+#include <seqan3/alignment/pairwise/alignment_result.hpp>
 #include <seqan3/core/algorithm/configuration.hpp>
 #include <seqan3/core/detail/empty_type.hpp>
 #include <seqan3/core/detail/template_inspection.hpp>
@@ -24,28 +28,48 @@ namespace seqan3::detail
 /*!\brief Implements the alignment result builder.
  * \ingroup pairwise_alignment
  *
- * \tparam alignment_configuration_t The type of the alignment configuration; must be a type specialisation of
- *                                   seqan3::configuration.
+ * \tparam result_t The pairwise alignment result type; must model seqan3::detail::template_specialisation_of
+ *                  seqan3::alignment_result.
  *
  * \details
  *
  * Implements the interfaces to build the alignment result based on the previously selected output configurations.
  */
-template <typename alignment_configuration_t>
-#if !SEQAN3_WORKAROUND_GCC_93467
+template <typename result_t>
 //!\cond
-    requires is_type_specialisation_of_v<alignment_configuration_t, configuration>
-//!\endcond
+#if !SEQAN3_WORKAROUND_GCC_93467
+    requires is_type_specialisation_of_v<result_t, alignment_result>
 #endif // !SEQAN3_WORKAROUND_GCC_93467
+//!\endcond
 class policy_alignment_result_builder
 {
 protected:
-    //!\brief The configuration traits type.
-    using traits_type = alignment_configuration_traits<alignment_configuration_t>;
+    // ----------------------------------------------------------------------------
+    // type defintions
+    // ----------------------------------------------------------------------------
+
     //!\brief The alignment result type.
-    using result_type = typename traits_type::alignment_result_type;
+    using result_type = result_t;
 
     static_assert(!std::same_as<result_type, empty_type>, "The alignment result type was not configured.");
+
+    //!\brief The internal data type storing the alignment information.
+    using result_data_type = typename alignment_result_value_type_accessor<result_type>::type;
+
+    // ----------------------------------------------------------------------------
+    // static member
+    // ----------------------------------------------------------------------------
+
+    //!\brief Flag indicating whether the trace matrix was computed.
+    static constexpr bool requires_trace_information = result_data_type::has_begin_positions ||
+                                                       result_data_type::has_alignment;
+    //!\brief Flag indicating whether the debug mode was enabled.
+    static constexpr bool is_debug = result_data_type::has_debug_score_matrix ||
+                                     result_data_type::has_debug_trace_matrix;
+
+    // ----------------------------------------------------------------------------
+    // member function
+    // ----------------------------------------------------------------------------
 
     /*!\name Constructors, destructor and assignment
      * \{
@@ -60,10 +84,13 @@ protected:
     /*!\brief Construction and initialisation using the alignment configuration.
      * \param[in] config The alignment configuration [not used in this context].
      */
+    template <typename alignment_configuration_t>
+    //!\cond
+        requires is_type_specialisation_of_v<alignment_configuration_t, configuration>
+    //!\endcond
     policy_alignment_result_builder(alignment_configuration_t const & SEQAN3_DOXYGEN_ONLY(config))
     {}
     //!\}
-
 
     /*!\brief Builds the seqan3::alignment_result based on the given alignment result type and then invokes the
      *        given callable with the result.
@@ -92,7 +119,7 @@ protected:
      */
     template <typename ...logger_specs_t, typename ...params_t>
     //!\cond
-        requires traits_type::is_debug
+        requires is_debug
     //!\endcond
     void make_result_and_invoke(policy_alignment_algorithm_logger<logger_specs_t...> & logger, params_t && ...params)
     {
@@ -102,7 +129,7 @@ protected:
     //!\overload
     template <typename ...params_t>
     //!\cond
-        requires (!traits_type::is_debug)
+        requires (!is_debug)
     //!\endcond
     void make_result_and_invoke(params_t && ...params)
     {
@@ -117,7 +144,7 @@ protected:
      * \tparam id_t The type of the id.
      * \tparam score_t The type of the score.
      * \tparam matrix_coordinate_t The type of the matrix coordinate.
-     * \tparam aligned_sequence_builder_t The type of the aligned sequences builder.
+     * \tparam trace_path_from_t The type of the aligned sequences builder.
      * \tparam callback_t The type of the callback to invoke.
      *
      * \param[in] logger The debug logger (only available when run in debug mode).
@@ -156,62 +183,54 @@ protected:
                                      [[maybe_unused]] aligned_sequence_builder_t const & aligned_sequence_builder,
                                      callback_t && callback)
     {
-        using std::get;
-        using invalid_t = std::nullopt_t *;
-
         result_type result{};
+        result_data_type & result_data = alignment_result_attorney::data(result);
 
-        if constexpr (traits_type::output_sequence1_id)
-            result.data.sequence1_id = id;
+        if constexpr (result_data_type::has_sequence1_id)
+            result_data.sequence1_id = id;
 
-        if constexpr (traits_type::output_sequence2_id)
-            result.data.sequence2_id = id;
+        if constexpr (result_data_type::has_sequence2_id)
+            result_data.sequence2_id = id;
 
-        if constexpr (traits_type::compute_score)
+        if constexpr (result_data_type::has_score)
+            result_data.score = std::move(score);
+
+        if constexpr (result_data_type::has_end_positions)
         {
-            static_assert(!std::same_as<decltype(result.data.score), invalid_t>,
-                          "Invalid configuration. Expected result with score!");
-            result.data.score = std::move(score);
+            result_data.end_positions.first = end_positions.col;
+            result_data.end_positions.second = end_positions.row;
         }
 
-        if constexpr (traits_type::compute_end_positions)
+        if constexpr (requires_trace_information)
         {
-            static_assert(!std::same_as<decltype(result.data.end_positions), invalid_t>,
-                          "Invalid configuration. Expected result with end positions!");
+            using std::get;
 
-            result.data.end_positions.first = end_positions.col;
-            result.data.end_positions.second = end_positions.row;
-        }
-
-        if constexpr (traits_type::requires_trace_information)
-        {
             auto aligned_sequence_result = aligned_sequence_builder(get<0>(sequence_pair),
                                                                     get<1>(sequence_pair),
                                                                     end_positions);
 
-            if constexpr (traits_type::compute_begin_positions)
+            if constexpr (result_data_type::has_begin_positions)
             {
-                result.data.begin_positions.first = aligned_sequence_result.first_sequence_slice_positions.first;
-                result.data.begin_positions.second = aligned_sequence_result.second_sequence_slice_positions.first;
+                result_data.begin_positions.first = aligned_sequence_result.first_sequence_slice_positions.first;
+                result_data.begin_positions.second = aligned_sequence_result.second_sequence_slice_positions.first;
             }
 
-            if constexpr (traits_type::compute_sequence_alignment)
-                result.data.alignment = std::move(aligned_sequence_result.alignment);
+            if constexpr (result_data_type::has_alignment)
+                result_data.alignment = std::move(aligned_sequence_result.alignment);
         }
 
         // In case we run in debug mode, we need to store the score and possibly trace matrix.
-        if constexpr (traits_type::is_debug)
+        if constexpr (is_debug)
         {
             // static constexpr that logger is not a ignore type.
             using std::swap;
-            swap(result.data.score_debug_matrix, logger.debug_score_matrix);
+            swap(result_data.score_debug_matrix, logger.debug_score_matrix);
 
-            if (traits_type::compute_sequence_alignment)
-                swap(result.data.trace_debug_matrix, logger.debug_trace_matrix);
+            if (result_data_type::has_alignment)
+                swap(result_data.trace_debug_matrix, logger.debug_trace_matrix);
         }
 
         callback(std::move(result));
     }
-
 };
 } // namespace seqan3::detail
